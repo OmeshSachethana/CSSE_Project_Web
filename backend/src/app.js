cdcd7d07becac9a7990b66a6ab791f7722a3cf42
OmeshSachethana <sachethana12@gmail.com>
2023-09-17 21:50:53 +0530

    created server connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-const User = require('./config');
+
 const app = express();
-
 app.use(express.json());
-app.use(cors());
-//const port = 3000
-
-app.post('/create', async (req, res) => {
-    const data = req.body;
-    console.log("Data of Users", data);
-    await User.add(data);
-    res.send({msg: "User Added!"});
-});
 
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(4000, () => console.log(`Example app listening on port 4000!`))
+app.listen(3000, () => console.log('Server started on port 3000'));
