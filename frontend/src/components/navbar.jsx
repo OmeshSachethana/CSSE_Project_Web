83988b96791067d0266b217d7476caba126fe048
OmeshSachethana <sachethana12@gmail.com>
2023-09-27 21:01:21 +0530

    hover added

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,15 +5,14 @@ const Navbar = () => {
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
       <h1 className="text-white text-2xl font-semibold">Constro</h1>
       <ul className="flex space-x-8">
-        <li><a href="#" className="text-white">Home</a></li>
-        <li><a href="#" className="text-white">Site Manager</a></li>
-        <li><a href="#" className="text-white">Accountant</a></li>
-        <li><a href="#" className="text-white">Supplier</a></li>
-        <li><a href="#" className="text-white">Manager</a></li>
-        <li><a href="#" className="text-white">Login</a></li>
+        <li><a href="#" className="text-white hover:text-blue-500">Home</a></li>
+        <li><a href="#" className="text-white hover:text-blue-500">Site Manager</a></li>
+        <li><a href="#" className="text-white hover:text-blue-500">Accountant</a></li>
+        <li><a href="#" className="text-white hover:text-blue-500">Supplier</a></li>
+        <li><a href="#" className="text-white hover:text-blue-500">Manager</a></li>
+        <li><a href="#" className="text-white hover:text-blue-500">Login</a></li>
       </ul>
     </nav>
-    
   );
 }
 
