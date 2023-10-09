fd0bc700a8054936855f2bf4ac7e2829357461ff
OmeshSachethana <sachethana12@gmail.com>
2023-10-09 08:31:41 +0530

    fixed tailwindcss issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client'; // Updated import statement
+import './index.css'
 
 import App from './App'; // Assuming this is your main application component
 
