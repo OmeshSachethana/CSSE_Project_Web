e4f066dcea59bd2ff8d259c936bb392d0b664ea7
OmeshSachethana <sachethana12@gmail.com>
2023-10-09 12:07:11 +0530

    fixed id issue

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/HomePage';
 import SupplierList from '../components/Supplier/SupplierList';
-import SupplierForm from '../components/Supplier/AddSupplier';
 import SupplierUpdateForm from '../components/Supplier/UpdateSupplier';
+import SupplierForm from '../components/Supplier/CreateSupplier';
 
 const PageRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/suppliers" element={<SupplierList />} />
-      <Route path="/suppliers/new" element={<SupplierForm />} />
-      <Route path="/suppliers/update/:id" element={<SupplierUpdateForm />} />
+        <Route path="/suppliers/new" element={<SupplierForm />} />
+        <Route path="/suppliers/update/:id" element={<SupplierUpdateForm />} />
+
 
     </Routes>
   );
