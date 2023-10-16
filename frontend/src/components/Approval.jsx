653f2e842e69590486f1520385f76baeeeef6c1d
OmeshSachethana <sachethana12@gmail.com>
2023-10-16 13:01:07 +0530

    fixed not updaing issues with buttons

--- frontend/src/components/Approval.jsx
+++ frontend/src/components/Approval.jsx
@@ -49,6 +49,12 @@ const Approvals = () => {
                   </tr>
                 ))}
               </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="3" className="text-right font-bold">Total Price:</td>
+                  <td className="font-bold">{order.products.reduce((total, product) => total + (product.price * product.quantity), 0)}</td>
+                </tr>
+              </tfoot>
             </table>
             <div className="mt-4">
               <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleApprove(order.id)}>Approve</button>
