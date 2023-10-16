179acca76b71ed5734fd9c30cb9f9a25ee185127
OmeshSachethana <sachethana12@gmail.com>
2023-10-16 18:23:01 +0530

    userActions created

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,23 @@
+// src/user/actions/userActions.js
+import * as userService from '../services/userService';
+import { loginUser as login, registerUser as register } from '../slices/userSlice';
+
+
+
+export const loginUser = (userData) => async (dispatch) => {
+  try {
+    const user = await userService.loginUser(userData);
+    dispatch(login(user));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const registerUser = (userData) => async (dispatch) => {
+  try {
+    const user = await userService.registerUser(userData);
+    dispatch(register(user));
+  } catch (error) {
+    console.error(error);
+  }
+};
