7f3f9e894ebd51c3032dfc71f085e561dccb442a
OmeshSachethana <sachethana12@gmail.com>
2023-10-16 18:08:29 +0530

    added auth Router

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { loginUser, registerUser } = require('../controllers/authController');
+
+router.post('/login', loginUser);
+router.post('/register', registerUser);
+
+module.exports = router;
