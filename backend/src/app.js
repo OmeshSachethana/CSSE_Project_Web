2f3462ffbc367a224dee88b8df17a7bbf7affb32
OmeshSachethana <sachethana12@gmail.com>
2023-10-16 18:09:53 +0530

    created route

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,10 +10,12 @@ const userRoutes = require("./routes/userRoutes");
 const supplierRoutes = require("./routes/supplierRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const productRoutes = require("./routes/productRoutes");
+const authRoutes = require('./routes/authRoutes');
 
 app.use("/users", userRoutes);
 app.use("/suppliers", supplierRoutes);
 app.use("/orders", orderRoutes);
 app.use("/products", productRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(3000, () => console.log("Server started on port 3000"));
