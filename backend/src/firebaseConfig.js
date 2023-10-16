4b0f5e60c9a2f0504a843d3708a80e474378e794
OmeshSachethana <sachethana12@gmail.com>
2023-10-16 17:58:11 +0530

    reversed

--- backend/src/firebaseConfig.js
+++ backend/src/firebaseConfig.js
@@ -1,12 +1,10 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../serviceAccountKey.json');
 
-console.log('Initializing Firebase Admin SDK...');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
 
-module.exports = { admin, db };
+module.exports = db;
