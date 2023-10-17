3a5301c252a82fa48e2aa39200e788c111214d4f
OmeshSachethana <sachethana12@gmail.com>
2023-10-17 08:53:10 +0530

    added logo and designed login

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../actions/userActions';
+import logo from '../../assets/logo.png'; // Import your logo
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -12,21 +13,29 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="max-w-md mx-auto mt-10 p-6 h-150 bg-white rounded shadow-lg">
+      <img src={logo} alt="Logo" className="w-48 mx-auto mb-6" /> {/* Add the logo */}
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
       <input
         type="text"
+        className="w-full px-4 py-2 mb-4 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="password"
+        className="w-full px-4 py-2 mb-4 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleLogin}>Login</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
     </div>
   );
 };
