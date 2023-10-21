7e7497227af87f7b954c6f78ead1aa0acb1ae3e1
OmeshSachethana <sachethana12@gmail.com>
2023-10-21 09:53:31 +0530

    Added redux

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -1,46 +1,123 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createNewUser } from '../../actions/userActions';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import { Link, useNavigate } from 'react-router-dom';
 import firebase from '../../config';
-import { useNavigate } from 'react-router-dom';
 
-function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const RegisterForm = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    age: '',
+    phoneNumber: '',
+    address: '',
+  });
 
-  const signUp = async () => {
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      await firebase.auth().createUserWithEmailAndPassword(email, password);
+      const userCredential = await firebase.auth().createUserWithEmailAndPassword(formData.email, formData.password);
+      const user = userCredential.user;
+      
+      // Send a POST request to your Node.js server
+      await dispatch(createNewUser({
+        uid: user.uid,
+        age: formData.age,
+        phoneNumber: formData.phoneNumber,
+        address: formData.address
+      }));
+
       navigate('/'); // navigate to homepage
     } catch (error) {
-      console.error(error);
+      console.error('Error creating user:', error);
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
-      <div className="p-12 bg-white rounded shadow-xl w-96">
-        <h1 className="text-3xl font-bold mb-4">Sign Up</h1>
-        <input 
-          type="email" 
-          onChange={e => setEmail(e.target.value)} 
-          className="w-full p-2 mb-6 text-primary border-b-2 border-primary outline-none focus:bg-gray-100"
-          placeholder="Email"
-        />
-        <input 
-          type="password" 
-          onChange={e => setPassword(e.target.value)} 
-          className="w-full p-2 mb-6 text-primary border-b-2 border-primary outline-none focus:bg-gray-100"
-          placeholder="Password"
-        />
-        <button 
-          onClick={signUp} 
-          className="w-full bg-primary hover:bg-secondary text-black font-semibold p-2 mt-5 rounded"
-        >
-          Sign Up
-        </button>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow mt-8">
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-m p-8">
+          <h2 className="text-xl mb-2 font-semibold">Sign Up</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Email"
+                required
+                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Password"
+                required
+                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="age"
+                value={formData.age}
+                onChange={handleChange}
+                placeholder="Age"
+                required
+                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                placeholder="Phone Number"
+                required
+                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+                placeholder="Address"
+                required
+                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
   );
-}
+};
 
-export default Register;
+export default RegisterForm;
