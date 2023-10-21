dd3bc1711ed259f59a9e98f3cd14bba97ae3e618
OmeshSachethana <sachethana12@gmail.com>
2023-10-21 08:11:52 +0530

    changed Login

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,43 +1,26 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { loginUser } from '../../actions/userActions';
-import logo from '../../assets/logo.png'; // Import your logo
+import firebase from '../../config';
 
-const Login = () => {
-  const dispatch = useDispatch();
+function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    dispatch(loginUser({ email, password }));
+  const signIn = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      console.log('Signed In');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 h-150 bg-white rounded shadow-lg">
-      <img src={logo} alt="Logo" className="w-48 mx-auto mb-6" /> {/* Add the logo */}
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <input
-        type="text"
-        className="w-full px-4 py-2 mb-4 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        className="w-full px-4 py-2 mb-4 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={handleLogin}
-      >
-        Login
-      </button>
+    <div>
+      <input type="email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={signIn}>Sign In</button>
     </div>
   );
-};
+}
 
 export default Login;
