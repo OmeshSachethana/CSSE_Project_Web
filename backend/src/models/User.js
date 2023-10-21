dc55b7ac5bbbe2c88ae57244868d5e1c91b7eff5
OmeshSachethana <sachethana12@gmail.com>
2023-10-21 09:47:39 +0530

    modified model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,11 +1,26 @@
-class User {
-    constructor(id, name, age, address) {
-      this.id = id;
-      this.name = name;
-      this.age = age;
-      this.address = address;
-    }
-  }
-  
-  module.exports = User;
-  
+// src/models/User.js
+
+const { db } = require('../firebaseConfig');
+
+exports.getAll = async () => {
+  const snapshot = await db.collection('users').get();
+  return snapshot.docs.map(doc => doc.data());
+};
+
+exports.getById = async (id) => {
+  const doc = await db.collection('users').doc(id).get();
+  return doc.exists ? doc.data() : null;
+};
+
+exports.create = async (userData) => {
+  const docRef = await db.collection('users').add(userData);
+  return docRef.id;
+};
+
+exports.update = async (id, newData) => {
+  await db.collection('users').doc(id).update(newData);
+};
+
+exports.delete = async (id) => {
+  await db.collection('users').doc(id).delete();
+};
